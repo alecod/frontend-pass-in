7d378d6ab6d1d2f48bdd18616f189ca04eb1add3
Alessandro Spoto <alecodx@gmail.com>
2024-05-24 21:23:25 -0300

    finish commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import { AttendeeList } from "./components/attendee-list";
 import { Header } from "./components/header";
-import { UserList } from "./components/user-list";
 
 export function App() {
   return (
     <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
-       <Header />
-       <UserList />
+      <Header />
+      <AttendeeList />
     </div>
-   
   )
 }
-
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
-import headerIcon from '../assets/header.svg'
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+import { NavLink } from './nav-link';
 
 export function Header() {
   return (
-    <div className='flex items-center gap-5 py-2'>
-      <img src={headerIcon} alt="icon development" />
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="NLW Unite" />
 
-      <nav className='flex items-center gap-5'>
-          <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
-          <a href="" className='font-medium text-sm'>Participantes</a>
+      <nav className="flex items-center gap-5">
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </div>
-  )
+  );
 }
